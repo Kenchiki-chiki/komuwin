ea46b1963d8fdcdfdec6158d5426940a2c5bec16
Kenchiki-chiki <kodama303@gmail.com>
2020-09-25 23:25:06 +0900

    管理者画面の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 EMAIL = 'test@example.com'
+ADMIN_EMAIL = 'admin@example.com'
 PASSWORD = 'password'
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email:EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
+end
+
+AdminUser.find_or_create_by!(email: EMAIL) do |user|
+  user.password = PASSWORD
+  puts '管理者の初期データインポートに成功しました。'
 end
